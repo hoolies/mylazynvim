33a6b8f05e7b5acb1323c675c694c8846d1555df
hoolies <c.rouvellas@gmail.com>
2025-07-15 21:17:46 -0400

    Fix the colors,tokyonight-storm with custom highlights

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,25 +1,24 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    lazy = false,
-    transparent = true,
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require("tokyonight").setup({
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-          sidebars = "transparent",
-          floats = "transparent",
-        },
-      })
-      vim.cmd.colorscheme("tokyonight-storm")
-
-      -- Custom Colours
-      vim.cmd([[highlight LineNr guifg=#29465B]])
-      vim.cmd([[highlight CursorColumn guifg=#737CA1]])
-      vim.cmd([[highlight CursorLineNr guifg=#FF00FF]])
-      vim.cmd([[highlight IncSearch guibg=#FF00FF]])
-    end,
-  },
+  "folke/tokyonight.nvim",
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  lazy = false,
+  transparent = true,
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require("tokyonight").setup({
+      style = "storm",
+      styles = {
+        comments = { italic = false }, -- Disable italics in comments
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_highlights = function(hl)
+        hl.LineNr = { fg = "#29465B" }
+        hl.CursorColumn = { fg = "#737CA1" }
+        hl.CursorLineNr = { fg = "#FF00FF" }
+        hl.IncSearch = { bg = "#FF00FF" }
+      end,
+    })
+    vim.cmd([[colorscheme tokyonight-storm]])
+  end,
 }
